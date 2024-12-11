e345ca90ffa94baf78910f0c56be4c4ce1407d1e
BugHunter-X <askzenai@gmail.com>
2024-12-11 22:30:05 +0100

    Add bug.dart file

---
+++ bug.dart
@@ -0,0 +1,7 @@
+```dart
+List<int> numbers = [1, 2, 3, 4, 5];
+int sum = numbers.reduce((a, b) => a + b);
+print(sum);
+
+//This code will work correctly, however, if numbers is empty, calling reduce will throw an error.
+```
